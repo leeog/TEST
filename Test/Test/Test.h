f30602a458853ce07b31723efad9be84448772fe
apple <apple@liyongchao.local>
2017-09-18 11:24:41 +0800

    pp

---
+++ Test/Test/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Test
+//
+//  Created by apple on 2017/9/18.
+//  Copyright © 2017年 LiYongChao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Test : LiYongChao
+
+@end
---
+++ Test/Test/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Test
+//
+//  Created by apple on 2017/9/18.
+//  Copyright © 2017年 LiYongChao. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
